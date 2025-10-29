1714aa2baa8d24a2167641477f705c7a83c14791
Chia <angel6160@gmail.com>
2025-10-29 16:31:46 +0800

    update js

--- all.js
+++ all.js
@@ -1,47 +1,13 @@
-// let data = [
-//   {
-//     id: 0,
-//     name: "肥宅心碎賞櫻3日",
-//     imgUrl:
-//       "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-//     area: "高雄",
-//     price: 1400,
-//     group: 87,
-//     rate: 10,
-//     description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-//   },
-//   {
-//     id: 1,
-//     name: "貓空纜車雙程票",
-//     imgUrl:
-//       "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-//     area: "台北",
-//     price: 240,
-//     group: 99,
-//     rate: 2,
-//     description:
-//       "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-//   },
-//   {
-//     id: 2,
-//     name: "台中谷關溫泉會1日",
-//     imgUrl:
-//       "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-//     area: "台中",
-//     price: 1765,
-//     group: 20,
-//     rate: 7,
-//     description:
-//       "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-//   },
-// ];
-
 let data = [];
 const url =
   "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
+const cardArea = document.querySelector(".card-area");
+const filter = document.querySelector(".result-area");
+let dataNum = document.querySelector(".dataNum");
+let str = "";
 
 function getData() {
-  console.log(axios);
+  // console.log(axios);
   axios.get(url).then(function (response) {
     // console.log("資料有回傳了");
     data = response.data.data;
@@ -52,8 +18,7 @@ function getData() {
 }
 getData();
 
-const cardArea = document.querySelector(".card-area");
-
+// 初始化邏輯
 function init() {
   // 卡片內容排序邏輯
   let str = "";
@@ -84,17 +49,13 @@ function init() {
             </div>
           </div>`;
     str += content;
-    console.log(str);
+    // console.log(str);
     cardArea.innerHTML = str;
   });
 }
 init();
 
 // 搜尋篩選邏輯
-const filter = document.querySelector(".result-area");
-let dataNum = document.querySelector(".dataNum");
-let str = "";
-
 function renderData() {
   filter.addEventListener("change", function (e) {
     //console.log(e.target.value);
@@ -182,7 +143,6 @@ function renderData() {
 }
 
 // 新增旅遊資料邏輯
-
 const ticketName = document.querySelector("#name");
 const imgUrl = document.querySelector("#imgUrl");
 const area = document.querySelector("#area");
@@ -209,7 +169,6 @@ btn.addEventListener("click", function (e) {
   e.preventDefault();
 
   let obj = {};
-
   // 驗證表單空欄
   alertMessages.forEach((item) => {
     item.innerHTML = "";
@@ -217,7 +176,7 @@ btn.addEventListener("click", function (e) {
   let isFormValid = true; // 驗證是否為空
   formInputs.forEach((item, index) => {
     const value = item.value;
-    if (value.trim() == "") {
+    if (value.trim() == "" || value === "請選擇景點地區") {
       alertMessages[index].innerHTML = `
                 <i class="bi bi-exclamation-circle-fill"></i>
                 <p class="fs-sm">此欄必填！</p>
@@ -231,32 +190,13 @@ btn.addEventListener("click", function (e) {
 
   idNum += 1;
   obj.id = idNum;
-  obj.name = ticketName.value;
-  obj.imgUrl = imgUrl.value;
+  obj.name = ticketName.value.trim();
+  obj.imgUrl = imgUrl.value.trim();
   obj.area = area.value;
-  obj.price = price.value;
-  obj.group = group.value;
-  obj.rate = rate.value;
-  obj.description = description.value;
-
-  // 驗證表單空欄 <-如果用querySelector寫只會驗證到第一欄，但要驗證所有欄位
-  // if (
-  //   name.value == "" ||
-  //   imgUrl.value == "" ||
-  //   area.value == "" ||
-  //   price.value == "" ||
-  //   group.value == "" ||
-  //   rate.value == "" ||
-  //   description == ""
-  // ) {
-  //   alertMessage.innerHTML = `
-  //             <img src="./img/alert.svg" alt="alert-icon">
-  //             <p class="fs-sm">此欄必填！</p>
-  //             `;
-  //   return;
-  // }
-  // alertMessage.innerHTML = "";
-
+  obj.price = Number(price.value);
+  obj.group = Number(group.value);
+  obj.rate = Number(rate.value);
+  obj.description = description.value.trim();
   data.push(obj);
   init();
   ticketName.value = "";
@@ -268,4 +208,4 @@ btn.addEventListener("click", function (e) {
   description.value = "";
 });
 
-// console.log(data); 檢查用
+// console.log(data);
